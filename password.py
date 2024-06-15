daafb8fd6bddef17dd9be07ee40c144b7cd0a5d2
TAI-YU,WU <yoking0626@gmail.com>
2024-06-15 15:01:53 +0800

    finished password.py

--- password.py
+++ password.py
@@ -9,10 +9,14 @@ password = "a12345" # 儲存正確密碼
 i = 3 # 剩餘機會
 
 while i > 0: # 剩餘機會為 1~3 時進入迴圈
+	i = i - 1 # 剩餘次數 - 1 後再存回去 i
 	pwd = input("請輸入密碼：") # 儲存使用者輸入
 	if pwd == password:
 		print("登入成功！")
 		break # 逃出迴圈
 	else:
-		i = i - 1 # 剩餘次數 - 1 後再存回去 i
-		print("密碼錯誤！ 還有 ", i, " 次機會！")
+		print("密碼錯誤！")
+		if i > 0: # 剩餘機會為 1~2 時
+			print("還有 ", i, " 次機會！")
+		else:
+			print("沒機會嘗試了！")
